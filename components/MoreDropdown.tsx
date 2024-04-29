9763739090182158fa0faf02d144dc5088eaef30
jwl <youremail@google.com>
2024-04-29 17:30:30 +0800

    feat: menubar

---
+++ components/MoreDropdown.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from './ui/button';
+import { Menu } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
+import { cn } from '@/lib/utils';
+
+const MoreDropdown = () => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Close the dropdown when the user clicks outside
+    function handleOutsideClick(event: MouseEvent) {
+      if (!event.target) return;
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        // setShowModeToggle(false);
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [ref]);
+
+  return (
+    <DropdownMenu open={open}>
+      <DropdownMenuTrigger asChild>
+        <Button
+          onClick={() => setOpen(!open)}
+          variant={'ghost'}
+          size={'lg'}
+          className="md:w-full !justify-start space-x-2 !px-3"
+        >
+          <Menu />
+          <div className="hidden lg:block">More</div>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        ref={ref}
+        className={cn(
+          'dark:bg-neutral-800 w-64 !rounded-xl !p-0 transition-opacity',
+          !open && 'opacity-0'
+        )}
+        align="end"
+        alignOffset={-40}
+      >
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default MoreDropdown;
